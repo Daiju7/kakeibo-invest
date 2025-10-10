a2eb5f5d97b5570ce988ec9143ff76c971400f37
Daiju Yokoyama <dai4580g@gmail.com>
2025-10-10 16:21:06 +0900

    コネクション確認

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mysql = require('mysql2');
 const app = express();
+const cors = require('cors');
 const PORT = 3000;
 
+app.use(express.json());
+app.use(cors());
 app.use(express.json());
 
 // MySQL接続設定
@@ -21,40 +24,28 @@ connection.connect(err => {
     }
 });
 
-// サンプルAPI
-app.get('/api/kakeibo', (req, res) => {
-    connection.query('SELECT * FROM kakeibo', (err, results) => {
-    if (err) return res.status(500).json({ error: err });
-    res.json(results);
-    });
-});
-
-
-app.get('/', (req, res) => {
-  res.send('家計簿APIサーバー稼働中');
-});
-
-
-app.post('/api/expenses', (req, res) => {
-    const { title, amount, date } = req.body;
-    
-    connection.query(
-        'INSERT INTO kakeibo (title, amount, date) VALUES (?, ?, ?)',
-        [title, amount, date],
-        (err, results) => {
-        if (err) return res.status(500).json({ error: err });
-        res.json({ id: results.insertId, title, amount, date });
-        }
-    );
-});
-
-
-
-
 // サーバー起動
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
+
+
+
+app.get('/', (req, res) => {
+    res.send('家計簿APIサーバー稼働中');
+});
+
+
+app.get('/a', (req, res) => {
+    return res.json('hello express')
+});
+
+
+
+
+
+
+
 
 
 
