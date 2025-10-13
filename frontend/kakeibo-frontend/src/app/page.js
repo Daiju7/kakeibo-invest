0231ebe72c90e729da4cd6b958150b6491d0a10d
Daiju Yokoyama <dai4580g@gmail.com>
2025-10-14 02:50:41 +0900

    フォームの入力と送信機能（コンソールへ）完成　

--- frontend/kakeibo-frontend/src/app/page.js
+++ frontend/kakeibo-frontend/src/app/page.js
@@ -1,19 +1,72 @@
 "use client";
 import { useEffect, useState } from "react";
 
-export default function Home() {
-  const [data, setData] = useState([]); //データベース状態管理
 
+
+export default function Home() {
+
+  const [data, setData] = useState([]); //データベース状態管理
+  const [form, setForm] = useState({ title: "", amount: "", date: "" });　//フォーム状態管理
+  
+  //データ取得 
   useEffect(() => {
-    fetch("http://localhost:3000/api/kakeibo")
-      .then((res) => res.json())
-      .then((result) => setData(result))
-      .catch((err) => console.error(err));
+    fetchData();
   }, []);
+
+  //データ取得関数 （データ取得初回ロード時だけでなく、データ追加・削除後にも実行したいため、非同期処理にする）
+  const fetchData = async() => {
+    try {
+      const response = await fetch("http://localhost:3000/api/kakeibo");
+      const result = await response.json();
+      setData(result);
+      console.log('取得できた');
+    } catch (error) {
+      console.log('Error fetching data:', error);
+    }
+  }
+
+
+  
 
   return (
     <main style={{ padding: "20px" }}>
       <h1>家計簿一覧表示</h1> 
+
+      <form
+        onSubmit={(e) => {
+          e.preventDefault(); // ページリロード防止
+          console.log(form);  // 今のフォームの内容を確認
+          //フォームの中をリセット
+          setForm({ title: "", amount: "", date: "" });
+        }}
+        style={{ marginBottom: "20px" }}
+      >
+        <input
+          type="text"
+          placeholder="項目名"
+          value={form.title}
+          onChange={(e) => setForm({ ...form, title: e.target.value })} 
+          //...はスプレッド構文、formの中身を全部展開して、その後titleだけ新しい値に更新。展開とは、オブジェクトや配列の中身を個別の要素に分解すること。
+          // e.target.valueは入力された値で、フォームの中身を更新するために使用される。
+          required //「空欄では送信できない」ようにする簡易バリデーション
+        />
+        <input
+          type="number"
+          placeholder="金額"
+          value={form.amount}
+          onChange={(e) => setForm({ ...form, amount: e.target.value })}
+          required
+        />
+        <input
+          type="date"
+          value={form.date}
+          onChange={(e) => setForm({ ...form, date: e.target.value })}
+          required
+        />
+        <button type="submit" style={{ marginLeft: "30px" }}>追加</button>
+      </form>
+
+
       {data.length === 0 ? (
         //条件式 ? Trueの処理 : Falseの処理
         <p>データがありません</p>
@@ -29,3 +82,5 @@ export default function Home() {
     </main>
   );
 }
+
+//次はpostリクエストの処理開発
